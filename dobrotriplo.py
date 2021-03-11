895c5217bc17bac9124c3826c31ae1cdd0f24f43
Lucas Lousada <80332417+luclous@users.noreply.github.com>
2021-03-11 15:56:20 -0300

    Dobro, Triplo, Raiz Quadrada

---
+++ dobrotriplo.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um valor: '))
+e1 = n1 * 2
+e2 = n1 * 3
+r = n1 ** (1/2)
+print('O dobro de {} é {}. \nO triplo de {} é {}. \nE a raiz quadrada de {} é {:.3f}'.format(n1, e1, n1, e2, n1, r))
