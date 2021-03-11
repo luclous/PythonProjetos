023054a98a6827318b63bf63313005b648a4bb8c
Lucas Lousada <lucas_lousada@hotmail.com>
2021-03-11 14:00:15 -0300

    Initial commit

---
+++ Somando2numeros.py
@@ -0,0 +1,5 @@
+print('Exercício somando dois números')
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
